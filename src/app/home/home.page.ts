6a1644e9dd02c0d1ac6984913834a62eeac59d71
Eliton Miguel Van Ryn <elitonvanryn@gmail.com>
2024-05-22 23:24:14 -0300

    Created main HTML structure

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  dadosFrete: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.dadosFrete = this.formBuilder.group({
+      pesoSaida: ['', Validators.required],
+      pesoChegada: ['', Validators.required],
+      diferenca: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log('Formulário enviado:', this.dadosFrete.value);
+    // Processar os valores do formulário aqui
+  }
 
 }
